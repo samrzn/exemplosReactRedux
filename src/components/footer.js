0a04943370aa84d432da7745045a095c3060b8a3
Samuel Rezende <sam_profissional@outlook.com>
2022-10-26 15:24:44 -0300

    Revert "transformando front-end HTML/CSS em ReactJS"
    This reverts commit 4f52cef96f3d6557296d93b44f8cf9907b02dd7f.

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function Rodape() {
+    return (
+        <footer className='Rodape'>
+            <h3>Formas de pagamento</h3>
+            <img src="./public/imagens/bandeiras-cartoes-credito.webp" alt="Formas de pagamento" />
+            <br />&copy; Recode PRO
+        </footer>
+    );
+}
+
+export default Rodape;
