64fd94890429d47d48a5fe512daf4cb5874dfac4
Samuel Rezende <sam_profissional@outlook.com>
2022-10-26 16:06:29 -0300

    restaurando projeto

--- frontend_react-redux/src/store/productStore.js
+++
@@ -1,4 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { productReducer } from '../reducer/productReducer';
-
-export const productStore = configureStore({ reducer: productReducer });
